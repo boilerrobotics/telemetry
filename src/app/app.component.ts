f00aca91d01b7793924ac007ce1009bee5d2bf69
tame0001 <thirawat.tam@gmail.com>
2024-01-21 15:19:04 -0500

    create mqtt subscription service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { Title } from "@angular/platform-browser";
-import { Observable } from "rxjs";
-import mqtt from "mqtt";
 import { NgxEchartsDirective, provideEcharts } from "ngx-echarts";
 
 @Component({
@@ -13,28 +11,12 @@ import { NgxEchartsDirective, provideEcharts } from "ngx-echarts";
   providers: [provideEcharts()],
   imports: [RouterOutlet, NgxEchartsDirective],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   private _title = "Boiler Robotics Telemetry";
-  private mqtt$!: Observable<string>;
 
-  constructor(private _titleService: Title) {
-    this.mqtt$ = new Observable<string>((observer) => {
-      const host = "ws://66.253.158.154:9001";
-      const client = mqtt.connect(host);
-      client.on("connect", () => {
-        client.subscribe("purdue-dac/#");
-      });
-      client.on("message", (topic, message, packet) => {
-        observer.next(message.toString());
-        // console.log(
-        //   "Received Message:= " + message.toString() + "\nOn topic:= " + topic
-        // );
-      });
-    });
-  }
+  constructor(private _titleService: Title) {}
 
   ngOnInit(): void {
     this._titleService.setTitle(this._title);
-    this.mqtt$.subscribe((data) => console.log(data));
   }
 }
---
+++ src/app/mqtt/mqtt.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import mqtt from "mqtt";
+import { MqttPayload } from "./mqtt.interface";
+
+@Injectable({
+  providedIn: "root",
+})
+export class MqttService {
+  private client!: mqtt.MqttClient;
+
+  constructor() {}
+
+  connect(hostUrl: string, port: Number): Promise<string> {
+    this.client = mqtt.connect(`ws://${hostUrl}:${port}`);
+    return new Promise<string>((resolve) => {
+      this.client.on("connect", () => {
+        resolve("Connected!");
+      });
+    });
+  }
+
+  subscribe(topic: string | string[]): Observable<MqttPayload> {
+    this.client.subscribe(topic, (err, granted) => {
+      console.log(`Subscribed to ${granted}`);
+    });
+
+    return new Observable<MqttPayload>((observer) => {
+      this.client.on("message", (topic, message, packet) => {
+        console.log(packet);
+        observer.next({
+          message: message.toString(),
+          topic: topic,
+        });
+      });
+    });
+  }
+}
