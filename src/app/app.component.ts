5bd85890a8f1581eb1e9f34bc265b07c4bfc42df
tame0001 <thirawat.tam@gmail.com>
2024-01-21 14:06:45 -0500

    create observable of mqtt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { Title } from "@angular/platform-browser";
+import { Observable } from "rxjs";
 import mqtt from "mqtt";
 import { NgxEchartsDirective, provideEcharts } from "ngx-echarts";
 
@@ -14,23 +15,26 @@ import { NgxEchartsDirective, provideEcharts } from "ngx-echarts";
 })
 export class AppComponent implements OnInit {
   private _title = "Boiler Robotics Telemetry";
+  private mqtt$!: Observable<string>;
 
-  constructor(private _titleService: Title) {}
+  constructor(private _titleService: Title) {
+    this.mqtt$ = new Observable<string>((observer) => {
+      const host = "ws://66.253.158.154:9001";
+      const client = mqtt.connect(host);
+      client.on("connect", () => {
+        client.subscribe("purdue-dac/#");
+      });
+      client.on("message", (topic, message, packet) => {
+        observer.next(message.toString());
+        // console.log(
+        //   "Received Message:= " + message.toString() + "\nOn topic:= " + topic
+        // );
+      });
+    });
+  }
 
   ngOnInit(): void {
     this._titleService.setTitle(this._title);
-    const host = "ws://66.253.158.154:9001";
-    console.log("connecting mqtt client");
-    const client = mqtt.connect(host);
-    console.log(client);
-    client.on("connect", () => {
-      console.log("client connected:");
-      client.subscribe("purdue-dac/#");
-    });
-    client.on("message", (topic, message, packet) => {
-      console.log(
-        "Received Message:= " + message.toString() + "\nOn topic:= " + topic
-      );
-    });
+    this.mqtt$.subscribe((data) => console.log(data));
   }
 }
